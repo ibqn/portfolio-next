1ca6141cad9a2e08268023a7e282422d986cc79d
ibqn <evgen.ibqn@gmail.com>
2022-09-25 10:21:22 +0200

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  plugins: [require("prettier-plugin-tailwindcss")],
+  tailwindConfig: "./tailwind.config.js",
+};
